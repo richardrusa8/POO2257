2e029535d1fd8ab83d0709839f0bba2b3633b4e1
richardrusa <72108799+richardrusa8@users.noreply.github.com>
2021-03-19 17:26:33 -0600

    Add files via upload

---
+++ ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conceptosbasicos;
+
+import ico.fes.Automovil;
+import java.awt.Color;
+
+/**
+ *
+ * @author richar
+ */
+public class ConceptosBasicos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Automovil miBocho=new Automovil();
+        //miBocho.marca="Volks Wagen";
+        miBocho.setMarca("VW");
+        miBocho.setSubmarca("Sedan");
+        miBocho.setModelo(1970);
+        miBocho.setColor(Color.blue);
+        System.out.println(miBocho);
+        
+    }
+    
+}
